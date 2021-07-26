4ffe73cf7507c6050c657a9b99cede0cc663ba8c
Kim Juho <sv05159@naver.com>
2021-07-26 11:41:35 +0900

    utils file upload

--- src/main/java/co/kr/circus/sauceweb/SauceProjectApplication.java
+++ src/main/java/co/kr/circus/sauceweb/SauceProjectApplication.java
@@ -1,13 +1,26 @@
 package co.kr.circus.sauceweb;
 
+import co.kr.circus.sauceweb.utils.fileupload.properties.StorageProperties;
+import co.kr.circus.sauceweb.utils.fileupload.service.FileSystemStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SauceProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SauceProjectApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(FileSystemStorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
