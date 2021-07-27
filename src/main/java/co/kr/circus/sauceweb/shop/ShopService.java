1e793ba6ea1759e7c009ca0e04f086e8541634ce
jaehak <cms893811@naver.com>
2021-07-27 23:15:47 +0900

    debugging

--- src/main/java/co/kr/circus/sauceweb/shop/ShopService.java
+++ src/main/java/co/kr/circus/sauceweb/shop/ShopService.java
@@ -1,22 +1,22 @@
-package co.kr.circus.sauceweb.shop;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ShopService {
-
-    private final ShopRepository shopRepository;
-
-    @Autowired
-    public ShopService(ShopRepository shopRepository) {
-        this.shopRepository = shopRepository;
-    }
-    /**
-     * 업체 등록
-     */
-    public Long write(Shop shop) {
-        shopRepository.save(shop);
-        return shop.getStore_id();
-    }
-}
+//package co.kr.circus.sauceweb.shop;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ShopService {
+//
+//    private final ShopRepository shopRepository;
+//
+//    @Autowired
+//    public ShopService(ShopRepository shopRepository) {
+//        this.shopRepository = shopRepository;
+//    }
+//    /**
+//     * 업체 등록
+//     */
+//    public Long write(Shop shop) {
+//        shopRepository.save(shop);
+//        return shop.getStore_id();
+//    }
+//}
