56178803d7769295f1064a1f188e03dfa9b5bee5
Kim Juho <sv05159@naver.com>
2021-08-11 17:25:50 +0900

    register store and update store info

---
+++ src/main/java/co/kr/circus/sauceweb/domain/store/StoreRepository.java
@@ -0,0 +1,6 @@
+package co.kr.circus.sauceweb.domain.store;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
---
+++ src/main/java/co/kr/circus/sauceweb/service/StoreService.java
@@ -0,0 +1,40 @@
+package co.kr.circus.sauceweb.service;
+
+import co.kr.circus.sauceweb.domain.store.Store;
+import co.kr.circus.sauceweb.domain.store.StoreRepository;
+import co.kr.circus.sauceweb.web.dto.StoreRegisterDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    @Transactional
+    public void save(StoreRegisterDto storeRegisterDto) {
+        storeRepository.save(Store.builder()
+                .storeName(storeRegisterDto.getStoreName())
+                .storePhone(storeRegisterDto.getStorePhone())
+                .bossName(storeRegisterDto.getBossName())
+                .address(storeRegisterDto.getAddress())
+                .number(storeRegisterDto.getNumber())
+                .build());
+    }
+
+    @Transactional
+    public void update(Long id, StoreRegisterDto storeRegisterDTO) {
+        Store store = storeRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+        );
+        store.update(storeRegisterDTO);
+    }
+
+    public Store findById(Long id) {
+        return storeRepository.findById(id).get();
+    }
+}
