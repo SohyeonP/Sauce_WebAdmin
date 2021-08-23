7288f809bba46d7424c188a2281167f34fb981b1
Kim Juho <sv05159@naver.com>
2021-08-24 03:55:45 +0900

    signup & login

--- src/main/java/co/kr/circus/sauceweb/web/BossController.java
+++ src/main/java/co/kr/circus/sauceweb/web/BossController.java
@@ -4,8 +4,8 @@ import co.kr.circus.sauceweb.service.BossService;
 import co.kr.circus.sauceweb.web.dto.MemberFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -14,14 +14,19 @@ public class BossController {
 
     private final BossService bossService;
 
+    @GetMapping("/login")
+    public String loginForm() {
+        return "loginForm";
+    }
+
     @GetMapping("/signup")
-    public String signup(Model model) {
-        model.addAttribute("memberFormDto", new MemberFormDto());
+    public String signupForm(@ModelAttribute MemberFormDto memberFormDto) {
+//        model.addAttribute("memberFormDto", new MemberFormDto());
         return "createMemberForm";
     }
 
     @PostMapping("/signup")
-    public String signup(MemberFormDto memberFormDto) {
+    public String signup(@ModelAttribute MemberFormDto memberFormDto) {
         bossService.signup(memberFormDto);
         return "redirect:/signup";
     }
