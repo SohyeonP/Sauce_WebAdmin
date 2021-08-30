a158be4505d251125cc785990dc1148825bfd88c
Kim Juho <sv05159@naver.com>
2021-08-30 23:11:40 +0900

    check username

--- src/main/java/co/kr/circus/sauceweb/web/BossController.java
+++ src/main/java/co/kr/circus/sauceweb/web/BossController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -33,5 +34,12 @@ public class BossController {
     public String signup(@ModelAttribute BossSignupDto bossSignupDto) {
         bossService.signup(bossSignupDto);
         return "redirect:/login";
+    }
+
+    @GetMapping("/idCheck")
+    @ResponseBody
+    public String idCheck(@ModelAttribute BossSignupDto bossSignupDto) {
+        String result = bossService.checkUsername(bossSignupDto.getUsername());
+        return result;
     }
 }
