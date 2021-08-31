b40fcc0868b04cd0821db378122103dcc0b734d8
Kim Juho <sv05159@naver.com>
2021-08-31 18:34:07 +0900

    update signup

--- src/main/java/co/kr/circus/sauceweb/web/dto/BossSignupDto.java
+++ src/main/java/co/kr/circus/sauceweb/web/dto/BossSignupDto.java
@@ -16,15 +16,19 @@ public class BossSignupDto {
     private String email;
     private String username;
     private String password;
+    private String check1;
+    private String check2;
 
     @Builder
-    public BossSignupDto(String name, String phone, String birth, String email, String username, String password) {
+    public BossSignupDto(String name, String phone, String birth, String email, String username, String password, String check1, String check2) {
         this.name = name;
         this.phone = phone;
         this.birth = birth;
         this.email = email;
         this.username = username;
         this.password = password;
+        this.check1 = check1;
+        this.check2 = check2;
     }
 
     public Boss toEntity(String encodedPassword) {
@@ -35,7 +39,16 @@ public class BossSignupDto {
                 .email(email)
                 .username(username)
                 .password(encodedPassword)
+                .check1(makeCheck(check1))
+                .check2(makeCheck(check2))
                 .build();
     }
 
+    public String makeCheck(String check) {
+        if (check == null) {
+            return "F";
+        } else {
+            return "T";
+        }
+    }
 }
