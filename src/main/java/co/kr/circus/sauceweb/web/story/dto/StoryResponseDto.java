891af378e1f8d8418b16096bfb4193a6ca65350c
Kim Juho <sv05159@naver.com>
2021-11-06 15:28:08 +0900

    add story dto

---
+++ src/main/java/co/kr/circus/sauceweb/web/story/dto/StoryResponseDto.java
@@ -0,0 +1,21 @@
+package co.kr.circus.sauceweb.web.story.dto;
+
+import co.kr.circus.sauceweb.domain.store.UploadFile;
+import co.kr.circus.sauceweb.domain.story.Story;
+import lombok.Getter;
+
+@Getter
+public class StoryResponseDto {
+
+    private Long id;
+    private UploadFile image;
+    private String title;
+    private String content;
+
+    public StoryResponseDto(Story entity) {
+        this.id = entity.getId();
+        this.image = entity.getImage();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+    }
+}
